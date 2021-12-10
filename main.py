7e79820e1c295268bf205b37345688f1e4806886
Taras <tarasnabitmn@gmail.com>
2021-12-10 17:00:05 +0200

    Add time

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import tkinter as tk
 from menu import MainMenu
 from play_button import PlayButton
 from play_field import PlayField
+from stopwatch import Stopwatch
 
 
 class App(tk.Tk):
@@ -16,12 +17,20 @@ class App(tk.Tk):
 
         play_button = PlayButton()
         play_button.pack()
+        play_button.configure(command=self.on_play_button_click)
 
         menu = MainMenu()
         self.config(menu=menu)
 
         play_field = PlayField(self.ROWS, self.COLUMNS)
         play_field.pack(pady=10)
+
+        self.stopwatch = Stopwatch()
+        self.stopwatch.pack()
+        self.stopwatch.start_watch()
+
+    def on_play_button_click(self):
+        self.stopwatch.reset()
 
 
 if __name__ == "__main__":
---
+++ stopwatch.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+
+
+class Stopwatch(tk.Label):
+    def __init__(self):
+        super().__init__(text="0", font=("Helvetica", 20), fg="black")
+
+    def start_watch(self):
+        now = self["text"]
+        next_value = int(now) + 1
+        self.configure(text=next_value)
+        self.after(1000, self.start_watch)
+
+    def reset(self):
+        self.configure(text="0")
