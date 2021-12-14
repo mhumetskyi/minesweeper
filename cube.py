b83bf8fe30454ed6ded44d717039875dabea292e
Dana <kim361866@gmail.com>
2021-12-14 21:56:38 +0200

    add cube and edit main.py and play_field.py

---
+++ cube.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+
+class Cube(tk.Button):
+    def __init__(self, master, x, y, number=0, *args, **kwargs):
+        super(Cube, self).__init__(master, width=6, height=3, *args, **kwargs)
+        self.x = x
+        self.y = y
+        self.number = number
+        self.mine = False
+        self.count_bomb = 0
+
+    def __repr__(self):
+        return f'Cube {self.x} {self.y} {self.number}, {self.mine}'
